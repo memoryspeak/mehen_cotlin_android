ace15e9ff015de65338f8473c760d5ed155455bd
nuxa <iliya.abdrakhimov@gmail.com>
2021-08-17 23:43:15 +0500

    set color

--- mehen/app/src/main/java/com/example/mehen/MehenView.kt
+++ mehen/app/src/main/java/com/example/mehen/MehenView.kt
@@ -5,7 +5,6 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.DisplayMetrics
 import android.view.View
 import kotlin.math.min
 
@@ -14,6 +13,10 @@ class MehenView(context: Context?, attrs: AttributeSet?) : View(context, attrs){
     private var originX = 20f
     private var originY = 200f
     private var cellSide = 130f
+    private val paint = Paint()
+    private val greenColor = Color.parseColor("#00FF00")
+    private val redColor = Color.parseColor("#FF1493")
+    private val lightColor = Color.parseColor("#FFDEAD")
 
     override fun onDraw(canvas: Canvas?) {
         canvas ?: return
@@ -23,9 +26,23 @@ class MehenView(context: Context?, attrs: AttributeSet?) : View(context, attrs){
         originX = (width - mehenBoardSide)/2f
         originY = (height - mehenBoardSide)/2f
 
-        val paint = Paint()
-        paint.color = Color.LTGRAY
+        drawMehenBoard(canvas)
 
-        canvas?.drawRect(originX, originY, originX + cellSide, originY + cellSide, paint)
+        //canvas?.drawRect(originX, originY, originX + cellSide, originY + cellSide, paint)
+    }
+
+    private fun drawMehenBoard(canvas: Canvas){
+        for (row in 0 until 8) {
+            for (col in 0 until 8) {
+                paint.color = redColor
+                canvas.drawRect(
+                    originX + col*cellSide,
+                    originY + row*cellSide,
+                    originX + (col+1)*cellSide,
+                    originY + (row+1)*cellSide,
+                    paint
+                )
+            }
+        }
     }
 }
